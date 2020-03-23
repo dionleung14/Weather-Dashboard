94feefe6b65d20cdf6ab7dd985cad6b8b43a4f53
NA Flipper <dioncleung@gmail.com>
2020-03-23 02:19:23 -0700

    changed css framework to tailwind. added functionality for searching a city

--- script.js
+++ script.js
@@ -2,12 +2,7 @@
 var submitBtn = $("#citySearchSubmit")
 
 submitBtn.click(function(event){
-    // Grab result of search bar
-    var searchResult = $("#citySearch").val()
-    event.preventDefault();
-    var starterURL = "api.openweathermap.org/data/2.5/weather?q="
-    var apiKey = "&appid=3a82cc89d61bd71079c6ba07c2e6fc75"
-    var queryURL = starterURL + searchResult + apiKey
+    var queryURL = "api.openweathermap.org/data/2.5/weather?q=seattle&appid=304be52fea59c58e48125f0ac059699be"
 
     $.ajax({
         url: queryURL,
@@ -19,7 +14,26 @@ submitBtn.click(function(event){
     })
 })
 
+// submitBtn.click(function(event){
+//     // Grab result of search bar
+//     var searchResult = $("#citySearch").val()
+//     event.preventDefault();
+//     var starterURL = "api.openweathermap.org/data/2.5/weather?q="
+//     var apiKey = "&appid=3a82cc89d61bd71079c6ba07c2e6fc75"
+//     var queryURL = starterURL + searchResult + apiKey
+
+//     $.ajax({
+//         url: queryURL,
+//         method: "GET"
+//     }).then(function(cityResult){
+//         console.log(cityResult)
+//         // var temperature = $("#temperature")
+//         // temperature.text(cityResult.main.temp)
+//     })
+// })
+
 // Weather url
 // api.openweathermap.org/data/2.5/weather?q={city name}&appid={your api key}
 
 // Weather API key: 3a82cc89d61bd71079c6ba07c2e6fc75
+// Weather API key: 04be52fea59c58e48125f0ac059699be
